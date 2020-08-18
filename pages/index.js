8de17181ae8ae8a2e2f9854fcfd1df2eeb5ef2e5
Alexey Malyshev <lordniken@mail.ru>
2020-08-18 18:34:07 +0300

    empty project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>Let's go!</h2>
+    </div>
+  )
+}
