6b9e3bd76e8b7a42e2838a23a76fa62b9a454800
Alexey Malishev <lordniken@mail.ru>
2020-08-21 15:42:07 +0300

    Filter added for cinema page

--- pages/cinema/[page].js
+++ pages/cinema/[page].js
@@ -1,21 +1,53 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-import { useRouter } from "next/router";
 import MovieItem from "../../components/cinema/movieItem";
 import Content from "../../components/content";
 import css from "../../css/cinema.module.css";
+import { useState, useEffect, useRef } from "react";
 
 export default function Cinema({ movies }) {
-  const router = useRouter();
+  const [filteredText, setFilteredText] = useState("");
+  const filterRef = useRef(null);
+
+  useEffect(() => {
+    filterRef.current.focus();
+  }, []);
+
+  const filteredMovies = () =>
+    filteredText
+      ? movies.filter(
+          (item) =>
+            item.title.toLowerCase().indexOf(filteredText.toLowerCase()) >= 0 ||
+            item.type.indexOf(filteredText) >= 0
+        )
+      : movies;
 
   return (
     <>
       <Header>Кинотеатр</Header>
-
       <Content>
+        <div className={css.filter_container}>
+          <div className={css.filter}>
+            <input
+              type="text"
+              className={css.input}
+              placeholder="Фильтр по названию или жанру"
+              ref={filterRef}
+              onChange={(e) => setFilteredText(e.target.value)}
+            />
+          </div>
+          <span className={css.filer_count}>
+            {filteredText &&
+              (filteredMovies().length
+                ? `Найдено ${filteredMovies().length} совпадений`
+                : "Совпадений не найдено")}
+          </span>
+        </div>
         <div className={css.container}>
           {movies &&
-            movies.map((item, index) => <MovieItem key={index} data={item} />)}
+            filteredMovies().map((item, index) => (
+              <MovieItem key={index} data={item} />
+            ))}
         </div>
       </Content>
       <Footer />
