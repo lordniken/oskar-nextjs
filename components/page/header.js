869d840e0581175420d00212bec90ea2836d6c6b
Alexey Malyshev <lordniken@mail.ru>
2020-08-24 01:09:06 +0300

    Css refactoring. Added support for most popular screen resolutions

--- components/page/header.js
+++ components/page/header.js
@@ -8,6 +8,10 @@ export default function Header({ children }) {
     <>
       <Head>
         <title>{children} / Кинотеатр "Оскар"</title>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
